59b9b9a94d7c4ff6ec4f7b056754806a062da590
Hasan jafferullah <hasan.jafferullah@ltts.com>
2021-08-05 22:05:37 +0530

    list icon change and text change

--- landing-page/process.php
+++ landing-page/process.php
@@ -1,12 +1,17 @@
 <?php
+
+error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[iOrbit SEO Enquiry]';
-$emailTo       = 'support@iorbit-tech.com'; //Replace Your Email Here..
+$emailTo       = 'abhisuri244@gmail.com'; //Replace Your Email Here..
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name    = stripslashes(trim($_POST['name']));
     $email   = stripslashes(trim($_POST['email']));
+
     // $phone = stripslashes(trim($_POST['phone']));
     // $message = stripslashes(trim($_POST['message']));
     if (empty($name)) {
@@ -26,7 +31,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $data['success'] = false;
         $data['errors']  = $errors;
     } else {
-        $subject = "$subjectPrefix $subject";
+        $subject = "$subjectPrefix";
         $body    = '
             <strong>Name: </strong>'.$name.'<br />
             <strong>Email: </strong>'.$email.'<br />
@@ -36,20 +41,36 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
             <strong>Email: </strong>'.$email.'<br />
             <strong>Subject: </strong>'.$phone.'<br />
         ';*/
-        $headers  = "MIME-Version: 1.1" . PHP_EOL;
-        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        $headers  = "MIME-Version: 1.0" . PHP_EOL;
+        $headers .= "Content-type: text/html" . PHP_EOL;
         $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
         $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
-        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
-        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
-		$headers .= "CC: karthikcg26@gmail.com" . PHP_EOL;
-		$headers .= "BCC: abhisuri244@gmail.com" . PHP_EOL;
-        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        // $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
+        $headers .= "From: support@iorbit-tech.com" . PHP_EOL;
+		// $headers .= "CC: karthikcg26@gmail.com" . PHP_EOL;
+		// $headers .= "BCC: abhisuri244@gmail.com" . PHP_EOL;
+        // $headers .= "Return-Path: $emailTo" . PHP_EOL;
         $headers .= "Reply-To: $email" . PHP_EOL;
         $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
-        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
-        mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
+        // $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
+
+        $headers2 = "MIME-Version: 1.0\r\n";
+        $headers2 .= "Content-type: text/html\r\n";
+        $headers2 .= 'From: karthikcg26@gmail.com' . "\r\n" .
+        'Reply-To: karthikcg26@gmail.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+        $headers3   = array();
+        $headers3[] = "MIME-Version: 1.0";
+        $headers3[] = "Content-type: text/plain; charset=utf-8";
+        $headers3[] = "From: karthik.cg@iorbit-tech.com"; 
+        $headers3[] = "X-Mailer: PHP/".phpversion();
+
+        // $status = mail($emailTo, $subject, $body, $headers);
+        // $status = mail("karthikcg26@gmail.com", "test", "test", $headers2);
+        $status = mail("karthikcg26@gmail.com", "subject", "message", implode("\r\n",$headers3));
         $data['success'] = true;
+        echo json_encode($status);
         $data['message'] = 'Thanks for your interest, will get back to you soon!';
     }
     // return all our data to an AJAX call
